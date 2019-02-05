741aff880895f94ed782568bf49db3b0b624a35b
omikhailk <13448403+omikhailk@users.noreply.github.com>
2019-02-05 20:00:42 +0300

    #1. First commit.

---
+++ problem7.py
@@ -0,0 +1,25 @@
+def is_prime(num):
+	trial_factor = 2
+	primes = []
+	while num != 1:
+		if num % trial_factor == 0:
+			primes.append(trial_factor)
+			num /= trial_factor
+		else:
+			trial_factor += 1
+		if len(primes) > 1:
+			return False
+	return True
+
+def prime_finder(index):
+	count = 1
+	current_prime = 2
+	current_number = 3
+	while count < index:
+		if is_prime(current_number) == True:
+			count += 1
+			current_prime = current_number
+		current_number += 2
+	return current_prime
+
+print(prime_finder(10001))
