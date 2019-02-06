9734337fb0d228112be0c5465ef9759979525606
omikhailk <13448403+omikhailk@users.noreply.github.com>
2019-02-06 22:43:39 +0300

    #1. First commit.

---
+++ problem10.py
@@ -0,0 +1,14 @@
+from helper import prime_sieve
+
+def sum_of_primes(limit):
+	"""
+	This function will use the Sieve of Eratosthenes
+	from the helper module and will generate a list
+	of primes uptill the limit that the user
+	provides.
+
+	Then it will return the sum of all those primes.
+	"""
+	return sum(prime_sieve(limit))
+
+print(sum_of_primes(2000000))
