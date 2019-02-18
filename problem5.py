520d7fd7e501a3f883f6f2d74b2bd23a6ee6778b
Omer Mikhail Khan <13448403+omikhailk@users.noreply.github.com>
2019-02-18 20:58:29 +0300

    2. Add docstring

--- problem5.py
+++ problem5.py
@@ -1,4 +1,9 @@
 def smallest_multiple(num_limit):
+	"""
+	This function will return the smallest
+	number that can be evenly divided by all
+	the numbers from 1 to and including num_limit.
+	"""
 	numbers = [i for i in range(1, num_limit + 1)]
 	starting_num = 2
 
