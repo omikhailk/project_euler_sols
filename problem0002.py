20557f292f6f4ae20961d5d593e29b6549032266
omikhailk <13448403+omikhailk@users.noreply.github.com>
2019-08-19 17:47:33 +0500

    4. Make PEP-8 compliant

--- problem0002.py
+++ problem0002.py
@@ -1,27 +1,29 @@
 def fib(term):
-	"""
-	This is a recursive fibonacci function.
-	"""
-	if term == 1:
-		return 1
-	if term == 2:
-		return 2
-	elif term > 1:
-		return fib(term - 1) + fib(term - 2)
+    """
+    This is a recursive fibonacci function.
+    """
+    if term == 1:
+        return 1
+    if term == 2:
+        return 2
+    elif term > 1:
+        return fib(term - 1) + fib(term - 2)
+
 
 def even_valued_terms(limit):
-	"""
-	This function will find and sum up all of
-	the even valued terms of the fibonacci
-	sequence as long as the terms are less
-	than or equal to the limit.
-	"""
-	even_values = []
-	i = 1
-	while fib(i) <= limit:
-		if fib(i) % 2 == 0:
-			even_values.append(fib(i))
-		i += 1
-	return sum(even_values)
+    """
+    This function will find and sum up all of
+    the even valued terms of the fibonacci
+    sequence as long as the terms are less
+    than or equal to the limit.
+    """
+    even_values = []
+    i = 1
+    while fib(i) <= limit:
+        if fib(i) % 2 == 0:
+            even_values.append(fib(i))
+        i += 1
+    return sum(even_values)
+
 
 print(even_valued_terms(4000000))
