335805355567852dd98111106ac7373a8fe9721b
omikhailk <13448403+omikhailk@users.noreply.github.com>
2019-08-19 17:39:34 +0500

    3. Use list comprehension

--- problem0001.py
+++ problem0001.py
@@ -1,12 +1,10 @@
-def three_or_five_multiples(limit):
-	"""
-	This function will find the sum of all of the
-	multiples of 3 or 5 which are below the limit.
-	"""
-	multiples = []
-	for i in range(1, limit):
-		if i % 3 == 0 or i % 5 == 0:
-			multiples.append(i)
-	return sum(multiples)
+def sum_of_multiples(limit):
+    """
+    This function will find the sum of all of the
+    multiples of 3 or 5 which are below the limit.
+    """
+    multiples = [i for i in range(1, limit) if not i % 3 or not i % 5]
+    return sum(multiples)
 
-print(three_or_five_multiples(1000))
+
+print(sum_of_multiples(1000))
