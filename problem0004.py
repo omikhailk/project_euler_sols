4f002abf5e67f25fdfe6e16090b681d24575c439
omikhailk <13448403+omikhailk@users.noreply.github.com>
2019-08-19 18:14:25 +0500

    3. Make PEP-8 compliant

--- problem0004.py
+++ problem0004.py
@@ -1,21 +1,22 @@
 def largest_palindrome_product(digits):
-	"""
-	This will return the largest palindromic number
-	made from the product of two N digit numbers.
-	"""
+    """
+    This will return the largest palindromic number
+    made from the product of two N digit numbers.
+    """
 
-	palindromes = []
-	lower_bounds = 10 ** (digits - 1)
-	upper_bounds = int("9" * digits)
+    palindromes = []
+    lower_bounds = 10 ** (digits - 1)
+    upper_bounds = int("9" * digits)
 
-	for i in range(upper_bounds, lower_bounds, -1):
-		for w in range(i, lower_bounds, -1):
-			number = str(i * w)
-			if number == number[::-1]:
-				palindromes.append(int(number))
-			palindromes.sort()
-			if len(palindromes) > 1:
-				palindromes = [palindromes[-1]]
-	return palindromes
+    for i in range(upper_bounds, lower_bounds, -1):
+        for w in range(i, lower_bounds, -1):
+            number = str(i * w)
+            if number == number[::-1]:
+                palindromes.append(int(number))
+            palindromes.sort()
+            if len(palindromes) > 1:
+                palindromes = [palindromes[-1]]
+    return palindromes
+
 
 print(largest_palindrome_product(3))
