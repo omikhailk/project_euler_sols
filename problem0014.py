49f01794c8bb6c3305c724df6465a93079919b96
omikhailk <13448403+omikhailk@users.noreply.github.com>
2019-08-23 03:01:09 +0500

    Initial commit

---
+++ problem0014.py
@@ -0,0 +1,26 @@
+def collatz(num):
+    original_num = num
+    iterations = 0
+    while num != 1:
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num = 3 * num + 1
+        iterations += 1
+    return (original_num, iterations)
+
+
+results = []
+start_num = 2
+while start_num < 1000000:
+    results.append(collatz(start_num))
+    start_num += 1
+
+max_iter = 1
+for i in results:
+    if i[1] > max_iter:
+        max_iter = i[1]
+
+for i in results:
+    if i[1] == max_iter:
+        print(i[0])
