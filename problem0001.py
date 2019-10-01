ddf61bb9450f5d2060f959e17f741b85a72e726f
omikhailk <13448403+omikhailk@users.noreply.github.com>
2019-10-02 02:30:13 +0500

    Add problem description and improve readability

--- problem0001.py
+++ problem0001.py
@@ -1,9 +1,14 @@
+"""
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+
 def sum_of_multiples(limit):
     """
     This function will find the sum of all of the
     multiples of 3 or 5 which are below the limit.
     """
-    multiples = [i for i in range(1, limit) if not i % 3 or not i % 5]
+    multiples = [i for i in range(1, limit) if not (i % 3 and i % 5)]
     return sum(multiples)
 
 
