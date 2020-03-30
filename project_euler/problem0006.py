96c156cddb91317cc351881d6e3a82f7d5d0d200
omikhailk <13448403+omikhailk@users.noreply.github.com>
2020-03-30 23:10:10 +0500

    Refactor and make docstring clearer
    - Use list comprehension

--- project_euler/problem0006.py
+++ project_euler/problem0006.py
@@ -7,19 +7,13 @@ natural numbers and the square of the sum.
 def sum_square_diff(limit_num):
     """
     This function will return the difference between
-    the sum of the squares of the numbers in
-    range(1, limit_num + 1), and the square of the sum of
-    the numbers range(1, limit_num + 1).
+    the sum of the squares and the square of the sums of the numbers
+    in the range 1 <= num <= limit_num
     """
-    square_of_sum = 0
-    numbers_list = [i for i in range(1, limit_num + 1)]
-    for number in numbers_list:
-        square_of_sum += number
-    square_of_sum = square_of_sum ** 2
+    square_of_sum = sum([i for i in range(1, limit_num + 1)])
+    square_of_sum **= 2
 
-    sum_of_squares = 0
-    for number in numbers_list:
-        sum_of_squares += number ** 2
+    sum_of_squares = sum([i ** 2 for i in range(1, limit_num + 1)])
 
     return square_of_sum - sum_of_squares
 
