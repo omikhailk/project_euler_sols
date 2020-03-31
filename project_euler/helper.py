e6c43d171f8381b30153c39fc6ad9db3c1037b6e
omikhailk <13448403+omikhailk@users.noreply.github.com>
2020-03-31 20:27:24 +0500

    Add prime sieve and upper bound calculator for the N-th prime
    - Add sieve of Eratosthenes and optimise it
    - Add relative imports for math.floor() and math.log()

--- project_euler/helper.py
+++ project_euler/helper.py
@@ -4,6 +4,7 @@ important functions which I may use again in
 other problems.
 """
 
+from math import floor, log
 
 # This decorator will allow the use of memoization.
 from functools import lru_cache
@@ -23,6 +24,55 @@ def fib(term):
         return 2
     elif term > 1:
         return fib(term - 1) + fib(term - 2)
+
+
+def upper_bound(nth):
+    """
+    Returns the term and the upper bound for the `nth`
+    prime number. This information is returned as a dictionary.
+
+    The upper bound was found from shorturl.at/aqBHQ
+    """
+    return {
+        "index": nth,
+        "upper_bound": floor(nth * (log(nth) + log(log(nth))))
+    }
+
+
+def prime_sieve(bound):
+    """
+    Uses the Sieve of Eratosthenes to generate primes up-to and
+    including the upper bound.
+    """
+    # The if-else below allows the function to either find primes up-till
+    # a N-th prime, or just find primes below a certain value.
+    # This allows usage of `upper_bound()` in conjunction with this function
+    if type(bound) == dict:
+        nth = bound["index"]
+        up_bound = bound["upper_bound"]
+    else:
+        up_bound = bound
+
+    nums = {i for i in range(3, up_bound, 2)} | {2}
+    # Added on the first prime of 2, since it wasn't included
+    # when counting the odd numbers
+    p = 3
+
+    while p ** 2 < up_bound:
+        p_mults = {i for i in range(p ** 2, up_bound, 2 * p)}
+        nums = nums.difference(p_mults)
+        # Removes all the marked numbers from `nums`
+        for x in sorted(list(nums)):
+            if x > p:
+                p = x
+                break
+        continue
+    if type(bound) == dict:
+        return sorted(list(nums))[:nth]
+        # This will give us the primes up-till the `nth` prime
+        # This is useful if we want the `nth` prime and only have an
+        # approximate upper bound
+    return sorted(list(nums))
 
 
 if __name__ == "__main__":
