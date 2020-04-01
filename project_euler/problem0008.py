0f64e0b3c463be99068eedfd8055733076d640a4
omikhailk <13448403+omikhailk@users.noreply.github.com>
2020-04-01 23:36:15 +0500

    Refactor code and remove input number to a different file
    - Use `prod` function from math library
    - Clean up docstring
    - Split functionality into `get_digits` and `greatest_product` functions

--- project_euler/problem0008.py
+++ project_euler/problem0008.py
@@ -4,56 +4,41 @@ greatest product. What is the value of this product?
 """
 
 
-def largest_product():
-    """
-    This code will look through the number below and try
-    and find the largest product that can be made
-    by multiplying 13 adjacent digits inside this 1000
-    digit number.
-    """
-    num = "73167176531330624919225119674426574742355349194934\
-96983520312774506326239578318016984801869478851843\
-85861560789112949495459501737958331952853208805511\
-12540698747158523863050715693290963295227443043557\
-66896648950445244523161731856403098711121722383113\
-62229893423380308135336276614282806444486645238749\
-30358907296290491560440772390713810515859307960866\
-70172427121883998797908792274921901699720888093776\
-65727333001053367881220235421809751254540594752243\
-52584907711670556013604839586446706324415722155397\
-53697817977846174064955149290862569321978468622482\
-83972241375657056057490261407972968652414535100474\
-82166370484403199890008895243450658541227588666881\
-16427171479924442928230863465674813919123162824586\
-17866458359124566529476545682848912883142607690042\
-24219022671055626321111109370544217506941658960408\
-07198403850962455444362981230987879927244284909188\
-84580156166097919133875499200524063689912560717606\
-05886116467109405077541002256983155200055935729725\
-71636269561882670428252483600823257530420752963450"
-    biggest_number = 0
-    results = []
-    for i in range(0, len(num) - 13):
-        product = 1
-        product_indexs = [int(num[w]) for w in range(i, i + 13)]
-        """
-        The code above will convert the 13 digit long substring
-        to a list with it's individual digits as integers, so
-        that we can do calculations with them.
+from math import prod
 
-        The code below will loop through that list and multiply
-        those numbers by the product, which is initially set to
-        1.
-        """
-        for z in product_indexs:
-            product *= z
-        if product != 0:
-            results.append(product)
-    return max(results)
+
+def get_digits(text_file):
+    """
+    Reads `text_file`, adds each line to a list
+    called `grid`, then flattens it and returns the whole list
+    in `line_nums`.
+    """
+    with open(text_file, "r") as data:
+        grid = [line.strip() for line in data]
+    line_nums = [num for sublist in grid for num in sublist]
+    # Flattens the 2D list
+    return line_nums
+
+
+def greatest_product(line_nums):
+    """
+    Takes `line_nums`, makes substrings of 13 digits which are then converted
+    into individual integers, which are multiplied to form a product which is
+    added to `result`. The product with the highest value is then returned
+    """
+    result = []
+    for index in range(0, len(line_nums) - 13):
+        # Uses `len(line_nums) - 13` as we only want 13 adjacent digits
+        # at a time
+        adj_digits = [int(num) for num in line_nums[index:index + 13]]
+        # Takes a substring of 13 numbers and then converts that to a list
+        # which each number is an integer
+        result.append(prod(adj_digits))
+    return max(result)
 
 
 def main():
-    print(largest_product())
+    print(greatest_product(get_digits("problem0008_input.txt")))
 
 
 if __name__ == '__main__':
