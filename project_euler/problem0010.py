36ac570320b85cf913489776c12ab947a3910b9e
omikhailk <13448403+omikhailk@users.noreply.github.com>
2020-04-01 16:01:55 +0500

    Fix up docstring

--- project_euler/problem0010.py
+++ project_euler/problem0010.py
@@ -8,12 +8,9 @@ from helper import prime_sieve
 
 def sum_of_primes(limit):
     """
-    This function will use the Sieve of Eratosthenes
+    Uses the prime sieve function
     from the helper module and will generate a list
-    of primes uptill the limit that the user
-    provides.
-
-    Then it will return the sum of all those primes.
+    of primes up-till `limit` and return the sum of the list
     """
     return sum(prime_sieve(limit))
 
