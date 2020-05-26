7a128ca0bdbafc530b0cdf160a868b1c06814cb8
omermikhailk <13448403+omermikhailk@users.noreply.github.com>
2020-05-27 04:21:12 +0500

    Add apostrophes around 'limit'
    - Makes it clear that it is a function argument
    that is being referenced

--- python/problem0001.py
+++ python/problem0001.py
@@ -6,7 +6,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 def sum_of_multiples(limit):
     """
     This function will find the sum of all of the
-    multiples of 3 or 5 which are below the limit.
+    multiples of 3 or 5 which are below `limit`.
     """
     multiples = [i for i in range(1, limit) if not (i % 3 and i % 5)]
     return sum(multiples)
