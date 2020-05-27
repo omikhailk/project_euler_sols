c3d4d2a7452f01453c8e7e776d37900c502cb736
omermikhailk <13448403+omermikhailk@users.noreply.github.com>
2020-05-27 08:42:23 +0500

    Clean docstring

--- python/problem0010.py
+++ python/problem0010.py
@@ -8,9 +8,9 @@ from helper import prime_sieve
 
 def sum_of_primes(limit):
     """
-    Uses the prime sieve function
-    from the helper module and will generate a list
-    of primes up-till `limit` and return the sum of the list
+    Uses the prime sieve function from the helper module and
+    will generate a list of primes up-till `limit` and return
+    the sum of the list.
     """
     return sum(prime_sieve(limit))
 
