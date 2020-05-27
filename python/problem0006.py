29137bcdb2abdbf9823dc275bc824d70b2350df0
omermikhailk <13448403+omermikhailk@users.noreply.github.com>
2020-05-27 08:24:03 +0500

    Clean up docstring and refactor code

--- python/problem0006.py
+++ python/problem0006.py
@@ -1,19 +1,18 @@
 """
-Find the difference between the sum of the squares of the first one hundred
-natural numbers and the square of the sum.
+Find the difference between the sum of the squares of the
+first one hundred natural numbers and the square of the sum.
 """
 
 
-def sum_square_diff(limit_num):
+def sum_square_diff(limit):
     """
-    This function will return the difference between
-    the sum of the squares and the square of the sums of the numbers
-    in the range 1 <= num <= limit_num
+    Returns the difference between the sum of the squares
+    and the square of the sums of the numbers in the
+    range 1 <= num <= limit.
     """
-    square_of_sum = sum([i for i in range(1, limit_num + 1)])
-    square_of_sum **= 2
+    square_of_sum = sum([i for i in range(1, limit + 1)]) ** 2
 
-    sum_of_squares = sum([i ** 2 for i in range(1, limit_num + 1)])
+    sum_of_squares = sum([i ** 2 for i in range(1, limit + 1)])
 
     return square_of_sum - sum_of_squares
 
