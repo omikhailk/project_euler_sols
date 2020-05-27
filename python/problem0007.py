ae7b8a9f5b2327f3d6a8c975ea5e11bd053b4219
omermikhailk <13448403+omermikhailk@users.noreply.github.com>
2020-05-27 08:31:53 +0500

    Fix helper module imports

--- python/problem0007.py
+++ python/problem0007.py
@@ -3,15 +3,15 @@ What is the 10,001st prime number?
 """
 
 
-from helper import upper_bound, prime_sieve
+from helper import prime_upper_bound, prime_sieve
 
 
 def nth_prime(n):
     """
-    Will return the last prime number (the `n`-th one) from the list of primes
-    generated using the `prime_sieve` function.
+    Will return the last prime number (the `n`-th one) from the
+    list of primes generated using the sieve.
     """
-    return prime_sieve(upper_bound(n))[-1]
+    return prime_sieve(prime_upper_bound(n))[-1]
 
 
 def main():
