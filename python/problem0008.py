ebadf2ad7bb332be30c9dbe67b9298deec74f09f
omermikhailk <13448403+omermikhailk@users.noreply.github.com>
2020-05-27 08:39:59 +0500

    Clean docstrings and comments

--- python/problem0008.py
+++ python/problem0008.py
@@ -9,14 +9,13 @@ from math import prod
 
 def get_digits(text_file):
     """
-    Reads `text_file`, adds each line to a list
-    called `grid`, then flattens it and returns the whole list
-    in `line_nums`.
+    Reads `text_file`, adds each line to a list called `grid`,
+    then flattens it and returns the whole list in `line_nums`.
     """
     with open(text_file, "r") as data:
         grid = [line.strip() for line in data]
-    line_nums = [num for sublist in grid for num in sublist]
     # Flattens the 2D list
+    line_nums = [num for sublist in grid for num in sublist]
     return line_nums
 
 
@@ -24,15 +23,15 @@ def greatest_product(line_nums):
     """
     Takes `line_nums`, makes substrings of 13 digits which are then converted
     into individual integers, which are multiplied to form a product which is
-    added to `result`. The product with the highest value is then returned
+    added to `result`.
+
+    The product with the highest value is then returned.
     """
     result = []
     for index in range(0, len(line_nums) - 13):
-        # Uses `len(line_nums) - 13` as we only want 13 adjacent digits
-        # at a time
-        adj_digits = [int(num) for num in line_nums[index:index + 13]]
         # Takes a substring of 13 numbers and then converts that to a list
         # which each number is an integer
+        adj_digits = [int(num) for num in line_nums[index:index + 13]]
         result.append(prod(adj_digits))
     return max(result)
 
