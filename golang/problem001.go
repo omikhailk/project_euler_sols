40cf5bc31fbff1187245171a872ed84a4856bc35
omermikhailk <13448403+omermikhailk@users.noreply.github.com>
2020-06-04 14:06:48 +0500

    Apply gofmt on file

--- golang/problem001.go
+++ golang/problem001.go
@@ -5,11 +5,11 @@ package main
 import "fmt"
 
 func main() {
-    var total int
-    for i := 1; i < 1000; i++ {
-        if i % 5 == 0 || i % 3 == 0 {
-            total += i
-        }
-    }
-    fmt.Println(total)
+	var total int
+	for i := 1; i < 1000; i++ {
+		if i%5 == 0 || i%3 == 0 {
+			total += i
+		}
+	}
+	fmt.Println(total)
 }
